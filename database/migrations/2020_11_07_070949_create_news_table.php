c928d3a08fbdb14f69687c514171d7cba731e608
andykey-krsk <andykey@bk.ru>
2020-11-07 22:39:52 +0700

    ДЗ_5 миграции

---
+++ database/migrations/2020_11_07_070949_create_news_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('short_text')->comment('Короткий текст новости');
+            $table->text('full_text')->comment('Полный текст новости');
+            $table->string('photo',1000)->nullable(false)->default('');
+            $table->foreignId('category_id')->constrained('ctegories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * 'id'
+    ''
+    ''
+    ''
+    'text'
+    'photo'
+     */
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
