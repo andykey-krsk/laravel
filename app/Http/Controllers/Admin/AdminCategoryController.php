ea67e7b2fbf009a5d21a6da16554949d465c806a
andykey-krsk <andykey@bk.ru>
2020-11-13 01:45:05 +0700

    ДЗ_6 сделал crud для рубрик, заказов, отзывов

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Response;
 
 class AdminCategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return Response::view('admin.category.index', [
+            'categories' => Category::query()->paginate(3),
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $category = Category::query()->findOrFail($id);
+        return Response::view('admin.category.edit', [
+            'category' => $category,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::query()->findOrFail($id);
+        $category->update($request->except('_token'));
+        return redirect()->route('category.index');
+    }
+
+    public function create()
+    {
+        return Response::view('admin.category.create');
+    }
+
+    public function store(Request $request)
+    {
+        Category::query()->create($request->except('_token'));
+        return redirect()->route('category.index');
+    }
 }
